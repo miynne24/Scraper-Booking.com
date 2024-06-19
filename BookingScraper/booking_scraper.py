96a65ee492c55763c0d086fa3b3a9638ed35f6ca
ABDUL MUHAIMIN <147019273+miynne24@users.noreply.github.com>
2024-06-19 23:26:23 +0800

    Update booking_scraper.py

--- BookingScraper/booking_scraper.py
+++ BookingScraper/booking_scraper.py
@@ -5,11 +5,12 @@ def main():
 
     with sync_playwright() as p:
 
-        destination = 'Pulau+Pinang'
+        destination = 'Kuala+Lumpur'
         checkin_date='2024-06-21'
         checkout_date='2024-06-23'
+        lang='en'
 
-        page_url=f'https://www.booking.com/searchresults.ms.html?ss={destination}&ssne={destination}&ssne_untouched={destination}&label=gen173nr-1FCAEoggI46AdIM1gEaKEBiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKX68izBsACAdICJGMxM2Q0NjE0LWRhZDEtNDY3Ni1iOThjLTNkNGEzZGFlMjg2ZdgCBeACAQ&sid=f9d0ad33086a56b2cc3cfecfe9bd03c7&aid=304142&lang=ms&sb=1&src_elem=sb&src=index&dest_id=&dest_type=city&checkin={checkin_date}&checkout={checkout_date}&group_adults=2&no_rooms=1&group_children=0'
+        page_url=f'https://www.booking.com/searchresults.html?ss={destination}&ssne={destination}&ssne_untouched={destination}&label=gen173nr-1FCAEoggI46AdIM1gEaKEBiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKX68izBsACAdICJGMxM2Q0NjE0LWRhZDEtNDY3Ni1iOThjLTNkNGEzZGFlMjg2ZdgCBeACAQ&sid=f9d0ad33086a56b2cc3cfecfe9bd03c7&aid=304142&lang={lang}&sb=1&src_elem=sb&src=index&dest_id=&dest_type=city&checkin={checkin_date}&checkout={checkout_date}&group_adults=2&no_rooms=1&group_children=0'
 
         browser=p.chromium.launch(headless=False)
         page=browser.new_page()
@@ -46,11 +47,19 @@ def main():
             hotel_dict['lodgingtype'] = hotel.locator('//h4[@class="b290e5dfa6 cf1a0708d9"]').inner_text()
             hotel_dict['url'] = hotel.locator('//a[@data-testid="availability-cta-btn"]').get_attribute('href')
 
+            # Extracting detailed deals information from the aria-label attribute
+            deals_element = hotel.locator('//span[@data-testid="property-card-deal"]')
+            if deals_element.count() > 0:
+                hotel_dict['deals'] = deals_element.get_attribute('aria-label')
+            else:
+                hotel_dict['deals'] = "No deals available"
+
+            hotel_dict['url'] = hotel.locator('//a[@data-testid="availability-cta-btn"]').get_attribute('href')
 
             hotels_list.append(hotel_dict)
 
         df = pd.DataFrame(hotels_list)
-        df.to_csv('PP_list_hotel.csv', index=False)
+        df.to_csv('KL_list_hotel.csv', index=False)
 
         browser.close()
 
